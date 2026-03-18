4d717e76e5ca2107043594699a73742bfd00754b
theunhackable <srirangasaipagala@gmail.com>
2026-03-18 20:34:38 +0530

    done things

--- main.go
+++ main.go
@@ -6,15 +6,16 @@ import (
 )
 
 func okBody(w http.ResponseWriter, req *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
 func main() {
 
 	fileServerPath := "."
+
 	mux := http.NewServeMux()
 
 	server := http.Server{
@@ -22,8 +23,12 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("GET /app/", http.StripPrefix("/app", http.FileServer(http.Dir(fileServerPath))))
-	mux.HandleFunc("GET /healthz", okBody)
+	cfg := &apiConfig{}
+
+	mux.Handle("GET /app/", http.StripPrefix("/app", cfg.middlewareMetricInc(http.FileServer(http.Dir(fileServerPath)))))
+	mux.HandleFunc("GET /admin/metrics", metricsHandler(cfg))
+	mux.HandleFunc("POST /admin/reset", resetHandler(cfg))
+	mux.HandleFunc("GET /api/healthz", okBody)
 
 	fmt.Printf("running server on http://localhost%v", server.Addr)
 	server.ListenAndServe()
